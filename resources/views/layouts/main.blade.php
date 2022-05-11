02391d12524455dc3863b24bec9a430456b4f4ac
Guxtaviko <Guxtaviko@hotmail.com>
2022-05-11 14:33:40 -0300

    Criação e Estilização do Formulário

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,15 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="{{ asset('/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}" >
 
     <title>DShape - Site para avaliações físicas</title>
   </head>
   <body>
     @yield('container')
 
-    <!-- Separate Popper and Bootstrap JS -->
-    <script src="{{ asset('/js/popper.min.js')}}"></script>
-    <script src="{{ asset('/js/bootstrap.min.js')}}"></script>
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="{{ asset('/js/bootstrap.bundle.min.js') }}"></script>
+    
   </body>
 </html>
--- resources/views/measurement/form.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('./layouts/main')
-
-@section('container')
-    <h1>Form</h1>
-@endsection
